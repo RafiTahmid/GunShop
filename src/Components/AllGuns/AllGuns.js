9019020216185f7f2906713556b51ad3d20549ae
RafiTahmid <rafi.tahmid019@gmail.com>
2022-10-27 23:33:55 +0600

    data fetched from json

--- src/Components/AllGuns/AllGuns.js
+++ src/Components/AllGuns/AllGuns.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const AllGuns = () => {
+  const [guns, setGuns] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://raw.githubusercontent.com/mir-hussain/guns/main/data.json`)
+      .then((res) => res.json())
+      .then((data) => setGuns(data));
+  }, []);
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Hello 2.28</h1>
     </div>
   );
 };
