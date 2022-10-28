5b1643d082899b8eed6faeda3d02881bc1ccf972
RafiTahmid <rafi.tahmid019@gmail.com>
2022-10-28 12:14:09 +0600

    card added"

--- src/Components/AllGuns/AllGuns.js
+++ src/Components/AllGuns/AllGuns.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import SingleGun from "../SingleGun/SingleGun";
 
 const AllGuns = () => {
   const [guns, setGuns] = useState([]);
@@ -9,9 +10,20 @@ const AllGuns = () => {
       .then((data) => setGuns(data));
   }, []);
   return (
-    <div>
-      <h1>Hello 2.28</h1>
-    </div>
+    <>
+      <h1 className="text-4xl font-bold mt-5">Welcome Hunters..!!!</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint harum
+        aliquam ratione porro minima alias voluptatem inventore error reiciendis
+        in?
+      </p>
+
+      <div className="w-[90%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-5">
+        {guns.map((gun) => (
+          <SingleGun gun={gun}></SingleGun>
+        ))}
+      </div>
+    </>
   );
 };
 
---
+++ src/Components/SingleGun/SingleGun.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const SingleGun = ({ gun }) => {
+  const { action, bullet, capacity, category, id, img, name, price } = gun;
+  return (
+    <div>
+      <div className="card w-full bg-base-100 shadow-xl">
+        <figure>
+          <img src={img} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            {name}
+            <div className="badge badge-secondary">NEW</div>
+          </h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline">Fashion</div>
+            <div className="badge badge-outline">Products</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleGun;
